c3d8b19ecf3452a543ab4a62a8c3766498a6b2ee
Harshal Dulera <111957198+harshaldulera@users.noreply.github.com>
2023-05-02 18:59:11 +0530

    Update IMDB.py

--- IMDB.py
+++ IMDB.py
@@ -1,46 +1,27 @@
-from bs4 import BeautifulSoup
+from imdb import Cinemagoer
 import requests
 import re
-import pandas as pd
+from bs4 import BeautifulSoup
 
-# Downloading imdb top 250 movie's data
-url = 'http://www.imdb.com/chart/top'
+# making imdb instance
+ia = Cinemagoer()
+
+#entering the name of the movie
+name = str(input("Enter name of the movie: "))
+
+#searching the movie
+search = ia.search_movie(name)
+
+movie_id = search[0].getID()
+
+print(movie_id)
+
+url = "https://www.imdb.com/title/tt" + movie_id
 response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-movies = soup.select('td.titleColumn')
-crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
-ratings = [b.attrs.get('data-value')
-		for b in soup.select('td.posterColumn span[name=ir]')]
+soup = BeautifulSoup(response, "html.parser")
+print(soup)
 
-# create a empty list for storing
-# movie information
-list = []
-
-# Iterating over movies to extract
-# each movie's details
-for index in range(0, len(movies)):
-	
-	# Separating movie into: 'place',
-	# 'title', 'year'
-	movie_string = movies[index].get_text()
-	movie = (' '.join(movie_string.split()).replace('.', ''))
-	movie_title = movie[len(str(index))+1:-7]
-	year = re.search('\((.*?)\)', movie_string).group(1)
-	place = movie[:len(str(index))-(len(movie))]
-	data = {"place": place,
-			"movie_title": movie_title,
-			"rating": ratings[index],
-			"year": year,
-			"star_cast": crew[index],
-			}
-	list.append(data)
-
-# printing movie details with its rating.
-for movie in list:
-	print(movie['place'], '-', movie['movie_title'], '('+movie['year'] +
-		') -', 'Starring:', movie['star_cast'], movie['rating'])
-
-##.......##
-df = pd.DataFrame(list)
-df.to_csv('imdb_top_250_movies.csv',index=False)
-
+# if not search:
+#     print("No results found")
+# else:
+#     print("ID of the first result", search[0].getID())
