cfda7e035594604c2c9f96d4a2fb69aa658d1f3f
[Harshal Dulera] <[harashaldulera82@gmail.com]>
2023-05-14 11:33:33 +0530

    Flask Update

--- main2.py
+++ main2.py
@@ -1,26 +1,51 @@
-from http import HTTPStatus
-from flask import Flask, render_template, request, json, Response
+from flask import Flask, render_template, request
 from imdb import Cinemagoer
-
-TITLE="MyApp"
-DEBUG=False
+import requests
+from bs4 import BeautifulSoup
+import json
 
 app = Flask(__name__)
-imdb = Cinemagoer()
 
-@app.route("/"):
-    def root():
-        return
-    render_template("index.html", title=TITLE)
-    
-@app.route("/search")
-def search():
-    title = request.args.get("title")
-    movies = imdb.search_movie(title)
-    if len(movies) == 0:
-        return Response(status=HTTPStatus.NOT_FOUND)
-    
-    movie = imdb.get_movie(movies[0].movieID)
-    return Response(response=json.dumps({"rating":movie["rating"]}), status=HTTPStatus.OK,)
+# Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-app.run(debug=DEBUG)
+# Route for processing the form submission
+@app.route('/result', methods=['POST'])
+def result():
+    # Creating IMDb instance
+    ia = Cinemagoer()
+
+    # Getting the movie name from the form submission
+    name = request.form['movie_name']
+
+    # Searching for the movie
+    search = ia.search_movie(name)
+
+    # Fetching movie ID
+    movie_id = search[0].getID()
+
+    url = "https://www.imdb.com/title/tt" + movie_id + "/"
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"})
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    data = json.loads(soup.find("script", type="application/ld+json").text)
+
+    # Fetching ratings from IMDb
+    rating = data["aggregateRating"]["ratingValue"]
+
+    # Fetching ratings from Rotten Tomatoes
+    url_rt = "https://www.rottentomatoes.com/m/" + name.replace(' ', '_')
+    response = requests.get(url_rt, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"})
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    data = json.loads(soup.find("script", type="application/ld+json").text)
+
+    rating2 = data["aggregateRating"]["ratingValue"]
+
+    # Render the result template with the ratings
+    return render_template('result.html', name=name, imdb_rating=rating, rt_rating=rating2)
+
+if __name__ == '__main__':
+    app.run()
