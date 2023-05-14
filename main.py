a5008c5c095b2ff64858b20814f93eb9b6cc0042
[Harshal Dulera] <[harashaldulera82@gmail.com]>
2023-05-14 11:29:08 +0530

    RottenTomatoes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from imdb import Cinemagoer
 import requests
-import re
+import re 
 from bs4 import BeautifulSoup
 import json
 
@@ -13,6 +13,7 @@ name = str(input("Enter name of the movie: "))
 #searching the movie
 search = ia.search_movie(name)
 
+# fetching movie ID
 movie_id = search[0].getID()
 
 print(movie_id)
@@ -21,9 +22,22 @@ url = "https://www.imdb.com/title/tt" + movie_id + "/"
 response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"})
 soup = BeautifulSoup(response.text, "html.parser")
 
+data = json.loads(soup.find("script", type="application/ld+json").text)
+
+# printing ratings from IMDB
+rating = data["aggregateRating"]["ratingValue"]
+print(rating)
+
+
+# fetching ratings from rottentomatoes
+url_rt = "https://www.rottentomatoes.com/m/" + name.replace(' ', '_')
+response = requests.get(url_rt, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"})
+soup = BeautifulSoup(response.text, "html.parser")
 
 data = json.loads(soup.find("script", type="application/ld+json").text)
 
-# print(response.text)
 rating2 = data["aggregateRating"]["ratingValue"]
-print(rating2)
+
+# printing ratings from rottentomatoes
+
+print(rating2 + '%')
